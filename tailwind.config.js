e06c271302218e9df06621ed2d2e458df33296ac
Prasanna <prasannavenkatesh918@gmail.com>
2022-09-21 12:15:19 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    
+    extend: {
+      colors: {
+        "dark-gray": '#1E1E1E',
+        "my-red": '#600202',
+      },
+    },
+  },
+  plugins: [],
+}
