dc9a7281c533ae9da46bd7c9b3e55a79974bd8bd
Prasanna <prasannavenkatesh918@gmail.com>
2022-09-27 20:16:48 +0530

    Scroll Animations added

--- src/script/main.js
+++ src/script/main.js
@@ -4,7 +4,7 @@ const delay = (delayInms) => {
 
 function ScrollToFrame2()
 {
-  document.querySelector('#bg-image2').scrollIntoView({
+  document.querySelector('#menu').scrollIntoView({
         behavior: 'smooth'
     });
     
@@ -13,8 +13,59 @@ function ScrollToFrame2()
     
 function ScrollToFrame1()
 {
-    document.querySelector('#bg-image1').scrollIntoView({
+    document.querySelector('#landing').scrollIntoView({
         behavior: 'smooth'
     });
 }
 
+let header = document.getElementById('landing-wrapper');
+
+function fadeOutOnScroll(element) {
+	if (!element) {
+        console.log('Div not found')
+		return;
+	}
+	
+	let distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+	let elementHeight = element.offsetHeight;
+	let scrollTop = document.documentElement.scrollTop;
+	
+	let opacity = 1;
+	
+	if (scrollTop > distanceToTop) {
+		opacity = 1 - (scrollTop - distanceToTop) / (elementHeight/2);
+	}
+	
+	if (opacity >= 0) {
+		element.style.opacity = opacity;
+	}
+}
+
+function scrollHandler() {
+	fadeOutOnScroll(header);
+}
+
+window.addEventListener('scroll', scrollHandler);
+
+const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.7
+  };
+  
+  function observerCallback(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // fade in observed elements that are in view
+        entry.target.classList.replace('fade-Out', 'fade-In');
+      } else {
+        // fade out observed elements that are not in view
+        entry.target.classList.replace('fade-In', 'fade-Out');
+      }
+    });
+  }
+  
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  
+  const fadeElms = document.querySelectorAll('#menu');
+  fadeElms.forEach(el => observer.observe(el));
