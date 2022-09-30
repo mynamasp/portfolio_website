4d2d6d34117124011b141350cab034c1ec8b1624
Prasanna <prasannavenkatesh918@gmail.com>
2022-09-30 20:59:00 +0530

    Cards Responsive Desgin Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {
       colors: {
         "dark-gray": '#1E1E1E',
